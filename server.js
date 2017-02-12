2640ba673ab91d397c94b44d5fb499b22e7b3cbc
coderunner09 <rampaysanjana8@gmail.com>
2017-02-12 23:34:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,11 +97,12 @@ app.get('/', function (req, res) {
   res.send(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/:articleName', function (req, res){
+    var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
-var articleName = req.params.articleName;
+
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
