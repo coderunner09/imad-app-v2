a1f89e1abfdcef3b283412a7705a24a074706053
coderunner09 <rampaysanjana8@gmail.com>
2017-02-12 23:40:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,8 +102,7 @@ app.get('/:articleName', function (req, res){
 });
 
 app.get('/ui/style.css', function (req, res) {
-
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
