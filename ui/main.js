161dead638a08e63322c40189984bcef61fda7f5
coderunner09 <rampaysanjana8@gmail.com>
2017-02-13 01:28:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,11 +4,11 @@ var img=document.getElementById('Madi');
 var marginRight=0;
 
 function moveRight() {
-marginRight = marginRight + 10;
+marginRight = marginRight + 1;
 img.style.marginRight = marginRight + 'px';
 }
 
 img.onclick = function(){
 
-    var interval = setInterval(moveRight, 100);
+    var interval = setInterval(moveRight, 50);
 }
