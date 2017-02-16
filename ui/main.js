365b53cb5919085ecbcb2c489d3c9b4dd0196baa
coderunner09 <rampaysanjana8@gmail.com>
2017-02-16 23:40:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,8 +4,32 @@ var button = document.getElementById('counter');
 var counter = 0; 
 
 button.onclick = function(){
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
     
-}
+    //make request to counter endpoint
+    
+    var request = new XMLHttprequest();
+    
+    
+    //Capture request
+    request.onreadystatechange = function(){ //checks current state of request object
+      if(request.readyState === XMLHttpRequest.DONE )
+      {
+          //Take some action
+          if(request.status===200){
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();           //Extract the value from request
+          }
+      }
+         
+         
+         //Not done yet
+    };
+    
+    
+    request.open('GET','http://coderunner09.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
+    //Make the request
+    
+};
