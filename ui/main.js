9dd70af5192082c67fb8f41c2ad7a026796e101d
coderunner09 <rampaysanjana8@gmail.com>
2017-02-20 17:50:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,7 +32,7 @@ button.onclick = function(){
     request.open('GET','http://coderunner09.imad.hasura-app.io/counter',true);
     request.send(null);
     
-    
+}; 
     var nameInput = document.getElementById('name');
     var name = nameInput.value;
     var submit = document.getElementById('submit_btn');
@@ -42,9 +42,9 @@ button.onclick = function(){
     var names = ['name1','name2','name3','name4'];
     var list = '';
     
-    for(var i=0;i<names.length;i++){
+    for(var i=0;i< names.length; i++) {
         
-        list += `<li>` + names[i] + `</li>`;
+        list += '<li>' + names[i] + '</li>';
     
         
     }
@@ -52,4 +52,3 @@ button.onclick = function(){
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
     };
-};
