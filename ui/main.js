1450442287b3c0d1a041d0b88e7d593c30472294
coderunner09 <rampaysanjana8@gmail.com>
2017-02-20 17:45:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -44,7 +44,7 @@ button.onclick = function(){
     
     for(var i=0;i<names.length;i++){
         
-        list += '<li>' + names[i] + '</li>';
+        list += `<li>` + names[i] + `</li>`;
     
         
     }
